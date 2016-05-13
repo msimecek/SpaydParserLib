fe0ad4ccdaa9e2752c5423fed036fcd96fad2631
msimecek <msimecek@microsoft.com>
2016-05-13 16:29:24 +0200

    Test for Mixed Spayd (containing Payment)

--- SpaydParserLibTests/SpaydTests.cs
+++ SpaydParserLibTests/SpaydTests.cs
@@ -45,12 +45,23 @@ namespace SpaydParserLib.Test
         {
             // Arrange
             string spaydString = "SPD*1.0*AM:9535.00*X-VS:1234567890*DT:20161216*CC:CZK*ACC:CZ3103000000270016060243*X-INV:SID%2A1.0%2AID:1963/160/2015%2ADD:20161201%2ATP:0%2AVII:CZ60194383%2AVIR:CZ12345678%2AINI:60194383%2ADUZP:20161201%2ATB0:1000.00%2AT0:210.00%2ATB1:6500.00%2AT1:975.00%2ANTB:850.00*";
+            Spayd expected = new Spayd()
+            {
+                Amount = 9535,
+                Vs = 1234567890,
+                Date = new System.DateTime(2016, 12, 16),
+                Currency = "CZK",
+                BankAccount = new BankAccount()
+                {
+                    Iban = "CZ3103000000270016060243"
+                }
+            };
 
             // Act
             var result = Spayd.FromString(spaydString);
 
             // Assert
-            Assert.IsNotNull(result);
+            Assert.AreEqual(expected, result);
         }
 
         [TestMethod]
