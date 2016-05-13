3222ad7c263e107492ed38fdab45207a439d6dd5
msimecek <msimecek@microsoft.com>
2016-05-13 16:22:50 +0200

    Basic test for mixed Spayd (containing payment)

--- SpaydParserLibTests/SpaydTests.cs
+++ SpaydParserLibTests/SpaydTests.cs
@@ -45,6 +45,19 @@ namespace SpaydParserLib.Test
         }
 
         [TestMethod]
+        public void Parse_Mixed()
+        {
+            // Arrange
+            string spaydString = "SPD*1.0*AM:9535.00*X-VS:1234567890*DT:20161216*CC:CZK*ACC:CZ3103000000270016060243*X-INV:SID%2A1.0%2AID:1963/160/2015%2ADD:20161201%2ATP:0%2AVII:CZ60194383%2AVIR:CZ12345678%2AINI:60194383%2ADUZP:20161201%2ATB0:1000.00%2AT0:210.00%2ATB1:6500.00%2AT1:975.00%2ANTB:850.00*";
+
+            // Act
+            var result = Spayd.FromString(spaydString);
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
         [ExpectedException(typeof(InvalidFormatException))]
         public void Parse_Invalid_MissingSPD()
         {
