26dca59d7e31b920d52a4db4c042012856bcd616
msimecek <msimecek@microsoft.com>
2016-06-02 15:00:23 +0200

    UWP support, metadata, NuGet

--- SpaydParserLib/Properties/AssemblyInfo.cs
+++ SpaydParserLib/Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("iSINDLib")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SpaydParserLib")]
+[assembly: AssemblyDescription("Parser for SPAYD and SIND")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("iSINDLib")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyProduct("SpaydParserLib")]
+[assembly: AssemblyCopyright("Copyright Martin Simecek, Miroslav Holec©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.2.0")]
+[assembly: AssemblyFileVersion("1.0.2.0")]
